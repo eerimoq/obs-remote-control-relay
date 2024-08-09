7a43623d1644cfb3a970ad3f03ce2c836be355c4
Erik Moqvist <erik.moqvist@gmail.com>
2024-08-09 22:15:36 +0200

    Refactoring.

--- static/index.js
+++ static/index.js
@@ -183,38 +183,6 @@ class Relay {
 let relay = undefined;
 let connections = [];
 
-function numberSuffix(value) {
-    return (value == 1 ? "" : "s");
-}
-
-function timeAgoString(fromDate) {
-    let now = new Date();
-    let secondsAgo = parseInt((now.getTime() - fromDate.getTime()) / 1000);
-    if (secondsAgo < 60) {
-        return `${secondsAgo} second${numberSuffix(secondsAgo)} ago`;
-    } else if (secondsAgo < 3600) {
-        let minutesAgo = parseInt(secondsAgo / 60);
-        return `${minutesAgo} minute${numberSuffix(minutesAgo)} ago`;
-    } else if (secondsAgo < 86400) {
-        let hoursAgo = parseInt(secondsAgo / 3600);
-        return `${hoursAgo} hour${numberSuffix(hoursAgo)} ago`;
-    } else {
-        return fromDate.toDateString();
-    }
-}
-
-function bitrateToString(bitrate) {
-    if (bitrate < 1000) {
-        return `${bitrate} bps`;
-    } else if (bitrate < 1000000) {
-        let bitrateKbps = (bitrate / 1000).toFixed(1);
-        return `${bitrateKbps} kbps`;
-    } else {
-        let bitrateMbps = (bitrate / 1000000).toFixed(1);
-        return `${bitrateMbps} Mbps`;
-    }
-}
-
 function reset(delayMs) {
     for (const connection of connections) {
         connection.close();
@@ -256,19 +224,6 @@ function resetSettings() {
     localStorage.setItem('obsPort', obsPort);
     populateObsPort();
     reset(0);
-}
-
-function getTableBody(id) {
-    let table = document.getElementById(id);
-    while (table.rows.length > 1) {
-        table.deleteRow(-1);
-    }
-    return table.tBodies[0];
-}
-
-function appendToRow(row, value) {
-    let cell = row.insertCell(-1);
-    cell.innerHTML = value;
 }
 
 function updateConnections() {
--- static/status.js
+++ static/status.js
@@ -45,51 +45,6 @@ function reset(delayMs) {
     }, delayMs);
 }
 
-function numberSuffix(value) {
-    return (value == 1 ? "" : "s");
-}
-
-function timeAgoString(fromDate) {
-    let now = new Date();
-    let secondsAgo = parseInt((now.getTime() - fromDate.getTime()) / 1000);
-    if (secondsAgo < 60) {
-        return `${secondsAgo} second${numberSuffix(secondsAgo)} ago`;
-    } else if (secondsAgo < 3600) {
-        let minutesAgo = parseInt(secondsAgo / 60);
-        return `${minutesAgo} minute${numberSuffix(minutesAgo)} ago`;
-    } else if (secondsAgo < 86400) {
-        let hoursAgo = parseInt(secondsAgo / 3600);
-        return `${hoursAgo} hour${numberSuffix(hoursAgo)} ago`;
-    } else {
-        return fromDate.toDateString();
-    }
-}
-
-function bitrateToString(bitrate) {
-    if (bitrate < 1000) {
-        return `${bitrate} bps`;
-    } else if (bitrate < 1000000) {
-        let bitrateKbps = (bitrate / 1000).toFixed(1);
-        return `${bitrateKbps} kbps`;
-    } else {
-        let bitrateMbps = (bitrate / 1000000).toFixed(1);
-        return `${bitrateMbps} Mbps`;
-    }
-}
-
-function getTableBody(id) {
-    let table = document.getElementById(id);
-    while (table.rows.length > 1) {
-        table.deleteRow(-1);
-    }
-    return table.tBodies[0];
-}
-
-function appendToRow(row, value) {
-    let cell = row.insertCell(-1);
-    cell.innerHTML = value;
-}
-
 function updateConnections(connections) {
     let body = getTableBody('connections');
     for (const connection of connections) {
